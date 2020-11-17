bf24cb41baba665df2282966b108a957dbbfe4f6
Richard <rrosenthal2013@gmail.com>
2020-11-17 17:55:13 +0000

    initial commit

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ print('Day 1 - Python Print Function')
 print('The function is declared like this:')
 print("print('what to print')")
 
+print('11-17-2020')
+print('Author: Richard Rosenthal')
+
 
 
 
